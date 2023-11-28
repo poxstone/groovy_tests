cd57a731a44f4ad35792998f00adf350f500560d
Your Name <you@example.com>
2023-11-27 19:22:03 -0500

    add examples

--- tests.groovy
+++ tests.groovy
@@ -1,9 +1,9 @@
 // https://learnxinyminutes.com/docs/es-es/groovy-es/
 
-def params = [
-  'dev': ['prj': 'vanti-data-operations-dev', 'sa_credentials': 'sa-cf-bq-raw-to-stg-dev@vanti-data-operations-dev.iam.gserviceaccount.com'],
-  'qal': ['prj': 'vanti-data-operations-qal', 'sa_credentials': 'sa-cf-bq-raw-to-stg-qal@vanti-data-operations-qal.iam.gserviceaccount.com'],
-  'prd': ['prj': 'vanti-data-operations-prd', 'sa_credentials': 'sa-cf-bq-raw-to-stg-prd@vanti-data-operations-prd.iam.gserviceaccount.com']
+def gcp_params = [
+  dev: [prj: 'test-data-operations-dev', sa_credentials: 'sa-cf-bq-raw-to-stg-dev@test-data-operations-dev.iam.gserviceaccount.com'],
+  qal: [prj: 'test-data-operations-qal', sa_credentials: 'sa-cf-bq-raw-to-stg-qal@test-data-operations-qal.iam.gserviceaccount.com'],
+  prd: [prj: 'test-data-operations-prd', sa_credentials: 'sa-cf-bq-raw-to-stg-prd@test-data-operations-prd.iam.gserviceaccount.com']
 ]
 
 def commando = "echo parametro ${params['dev']['sa_credentials']}"
